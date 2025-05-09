ea4d3941e154773e7555b92ca3893bc995163205
Victor-Valedev <victorvaledev@gmail.com>
2025-05-09 14:09:36 -0300

    feat: Cria esboço da tela de login, ainda não estilizado com tema

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Movie Review',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        textTheme: TextTheme(
+          titleMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          )
+        )
       ),
       home: const LoginPage(),
     );
--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -12,8 +12,105 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie Review'),
+        title: Text(
+          'Movie Review',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        backgroundColor: Color(0xFFE50914),
       ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'lib/assets/images/imageCapa.jpg',
+              fit: BoxFit.cover,
+            )
+          ),
+          Positioned(
+            top: 60,
+            left: 80,
+            right: 80,
+            child: Text(
+              'Veja detalhes dos filmes em lançamento, e muito mais!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            )
+          ),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 200),
+              padding: EdgeInsets.all(10),
+              child: Form(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Seja bem-vindo(a) ao seu app de filmes favorito!',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Text(
+                        'Faça login para continuar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(height: 15),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'E-mail:',
+                            prefixIcon: Icon(Icons.email),
+                            filled: true,
+                            fillColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Senha:',
+                            prefixIcon: Icon(Icons.password),
+                            filled: true,
+                            fillColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: (){}, 
+                          child: Text('Login')
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: (){}, 
+                        style: TextButton.styleFrom(
+                          textStyle: TextStyle(decoration: TextDecoration.underline)
+                        ),
+                        child: Text(
+                          'Não tem uma conta? Cadastre-se.',
+                          style: TextStyle(
+                            color: Colors.white70
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
