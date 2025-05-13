0d841c7eac99ed6c9dd2314680a0ab457c641dac
Victor-Valedev <victorvaledev@gmail.com>
2025-05-13 17:41:04 -0300

    feat: Realiza login dos usuários no firebase

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:moviesapp/exeptions/firebase_exceptions.dart';
+import 'package:moviesapp/service/auth_service.dart';
+import 'package:moviesapp/ui/home_page.dart';
 import 'package:moviesapp/utils/constants.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,6 +16,68 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController emailUser = TextEditingController();
   TextEditingController passwordUser = TextEditingController();
+  final AuthService _auth = AuthService();
+  bool _isLoading = false;
+
+  Future<void> login() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    if (!_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
+    try {
+      //Se der sucesso
+      await _auth.login(email: emailUser.text, password: passwordUser.text);
+
+      setState(() {
+        _isLoading = true;
+      });
+
+      if (!mounted) {
+        return;
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: [
+              Text(
+                'Login realizado com sucesso!',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              SizedBox(width: 8),
+              Icon(Icons.check, color: Colors.green),
+            ],
+          ),
+        ),
+      );
+
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (ctx) => HomePage()),
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      FirebaseExceptions.firebaseAuthErros(context, e);
+    }
+  }
+
+  @override
+  void dispose() {
+    emailUser.dispose();
+    passwordUser.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -147,11 +213,15 @@ class _LoginPageState extends State<LoginPage> {
                         width: double.infinity,
                         child: ElevatedButton(
                           onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                            }
+                            login();
                           },
                           style: Theme.of(context).elevatedButtonTheme.style,
-                          child: Text('Login'),
+                          child:
+                              _isLoading
+                                  ? CircularProgressIndicator(
+                                    color: Colors.white,
+                                  )
+                                  : Text('Login'),
                         ),
                       ),
                       TextButton(
