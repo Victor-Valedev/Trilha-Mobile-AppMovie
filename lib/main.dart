0919bdbaf8e4d63bd48bb3c6800a5b58d191df39
Victor-Valedev <victorvaledev@gmail.com>
2025-05-19 13:16:14 -0300

    feat: Restrita o app apenas para o modo retrato/portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:moviesapp/theme/app_theme.dart';
 import 'package:moviesapp/ui/home_page.dart';
@@ -12,6 +13,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await dotenv.load();
+
+  //Apenas modo portrait
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
